0943905f82657354401e8fadab7252f9788a2de4
JX3player <403401268@qq.com>
2017-09-07 17:22:37 +0800

    2017-09-07 push

--- js/index.js
+++ js/index.js
@@ -13,4 +13,33 @@ window.onload = function() {
     prevButton: '.swiper-button-prev',
    
   })        
+  
+	startMove();
+  
 }
+
+var myTime=null;
+var currLeft=0;
+
+function startMove(){
+	myTime=setInterval(goStep,20);
+	$("#alliances1").mouseenter(function(){
+		clearInterval(myTime);
+	});
+	$("#alliances1").mouseleave(function(){
+		clearInterval(myTime);
+		myTime = setInterval(goStep,20);
+	});
+}
+function goStep(){
+	currLeft=currLeft-2;
+	if(currLeft<-5474){
+		currLeft=0;
+	}
+	var a = currLeft+"px";
+	console.log(a)
+	$("#alliances1").css("left",a);
+}	
+
+
+	
